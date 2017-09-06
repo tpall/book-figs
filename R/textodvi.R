b9b892e5adc7d931b739ee63ba93da0acafd2f48
Taavi Päll <tapa741@gmail.com>
2017-09-06 22:48:46 +0300

    convert eps from pdf

---
+++ R/textodvi.R
@@ -0,0 +1,26 @@
+
+#' Save projects working directory
+projwd <- getwd()
+projwd
+
+#' Set working directory to tex files folder
+setwd("figs")
+
+#' List tex files and convert to dvi
+texfiles <- list.files(pattern = "tex")
+lapply(texfiles, tools::texi2dvi, clean = T, pdf = T)
+
+#' Convert pdf files to eps
+pdfiles <- list.files(pattern = "pdf")
+
+#' Function to wrap dvips
+pdf2psr <- function(x) {
+  x <- sub("\\.pdf", "", x)
+  system(sprintf("pdftops -eps %s.pdf %s.eps", x, x))
+}
+
+lapply(pdfiles, pdf2psr)
+
+#' Set working directory back to project wd
+setwd(projwd)
+projwd
